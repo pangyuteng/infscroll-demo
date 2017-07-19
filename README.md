a0299b800299c79bf668a553508730239a880a7a
pangyuteng <pangyuteng@gmail.com>
2017-07-18 19:49:55 -0700

    add nginfinite scroll

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ attempt of an single page app with infinite scroll capability using angular as f
 
 ref. 
 https://scotch.io/tutorials/creating-a-single-page-todo-app-with-node-and-angular
+https://sroze.github.io/ngInfiniteScroll/
 https://stackoverflow.com/questions/24689344/lazy-loading-more-data-scroll-in-mongoose-nodejs
 
 http://adrichman.github.io/implementing-a-lazy-load-and-infinite-scroll-in-angularjs/
--- public/core.js
+++ public/core.js
@@ -1,4 +1,4 @@
-var scotchTodo = angular.module('scotchTodo', []);
+var scotchTodo = angular.module('scotchTodo', ['infinite-scroll']);
 
 function mainController($scope, $http) {
 	$scope.formData = {};
@@ -37,3 +37,33 @@ function mainController($scope, $http) {
 	};
 
 }
+
+scotchTodo.controller('infController', function($scope, Reddit) {
+  $scope.reddit = new Reddit();
+});
+
+// Reddit constructor function to encapsulate HTTP and pagination logic
+scotchTodo.factory('Reddit', function($http) {
+  var Reddit = function() {
+    this.items = [];
+    this.busy = false;
+    this.after = '';
+  };
+
+  Reddit.prototype.nextPage = function() {
+    if (this.busy) return;
+    this.busy = true;
+
+    var url = "https://api.reddit.com/hot?after=" + this.after + "&jsonp=JSON_CALLBACK";
+    $http.jsonp(url).success(function(data) {
+      var items = data.data.children;
+      for (var i = 0; i < items.length; i++) {
+        this.items.push(items[i].data);
+      }
+      this.after = "t3_" + this.items[this.items.length - 1].id;
+      this.busy = false;
+    }.bind(this));
+  };
+
+  return Reddit;
+});
