f63797cbf23c17182b7a20fec065039d426ee0af
pangyuteng <pangyuteng@gmail.com>
2017-07-20 22:20:14 -0700

    working ver

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
 
 	// api ---------------------------------------------------------------------
 	// get all todos
-	app.get('/api/todosD', function(req, res) {
+	app.get('/api/todos', function(req, res) {
 
 		// use mongoose to get all todos in the database
 		Todo.find(function(err, todos) {
